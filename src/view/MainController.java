6fadb5e8abcc1422989aa5e16bc8fa5a9e969aa3
Steven <steven.sell1@gmail.com>
2017-03-20 12:36:30 -0400

    More Functionality Added

--- src/view/MainController.java
+++ src/view/MainController.java
@@ -10,11 +10,30 @@ public class MainController {
 
     @FXML
     private Label display;
+    @FXML
+    private Button clear;
+    @FXML
+    private Button posNeg;
+    @FXML
+    private Button percent;
+    @FXML
+    private Button div;
+    @FXML
+    private Button mult;
+    @FXML
+    private Button sub;
+    @FXML
+    private Button add;
+    @FXML
+    private Button equal;
 
     // Reference to the main application.
     private Main main;
-    private int lastNum = 0;
-    private String num = "";
+    private double lastNum = 0;
+    private String curr = "0";
+    private String lastFunct = "";
+    private String buffer = "";
+    private boolean isPercent = false;
 
     public MainController() {
     }
@@ -28,53 +47,83 @@ public class MainController {
         this.main = main;
     }
 
-    private void handleAddition() {
-    	int tempNum = lastNum + Integer.parseInt(num);
-        String text = Integer.toString(tempNum);
+    @FXML
+    private void handleAdd() {
+    	double tempNum = lastNum + Double.parseDouble(buffer);
+        String text = Double.toString(tempNum);
     	display.setText(text);
+    	curr = text;
         lastNum = tempNum;
-    }
-    
-    private void handleSub() {
-    	int tempNum = lastNum - Integer.parseInt(num);
-        String text = Integer.toString(tempNum);
-    	display.setText(text);
-        lastNum = tempNum;
-    }
-    
-    private void handleMult() {
-    	int tempNum = lastNum*Integer.parseInt(num);
-        String text = Integer.toString(tempNum);
-    	display.setText(text);
-        lastNum = tempNum;
-    }
-    
-    private void handleDiv() {
-    	int tempNum = lastNum/Integer.parseInt(num);
-        String text = Integer.toString(tempNum);
-    	display.setText(text);
-        lastNum = tempNum;
+        buffer = "0";
     }
     
     @FXML
-    private void handleButton(ActionEvent evt) {
-    	Button button = (Button)evt.getSource();
-    	if(button.getText().equals("+")) {
-    		handleAddition();
-    		num = "";
-    	} else if(button.getText().equals("*")) {
-    		handleMult();
-    		num = "";
-    	} else if(button.getText().equals("/")) {
-    		handleDiv();
-    		num = "";
-    	} else if(button.getText().equals("AC")) {
-    		lastNum = 0;
-    		num = "";
-    	} else if(button.getText().equals("=")) {
-    		display.setText(num);
+    private void handleSub() {
+    	double tempNum = lastNum - Double.parseDouble(buffer);
+        String text = Double.toString(tempNum);
+    	display.setText(text);
+    	curr = text;
+        lastNum = tempNum;
+        buffer = "0";
+    }
+    
+    @FXML
+    private void handleMult() {
+    	double tempNum = lastNum*Double.parseDouble(buffer);
+        String text = Double.toString(tempNum);
+    	display.setText(text);
+    	curr = text;
+        lastNum = tempNum;
+        buffer = "0";
+    }
+    
+    @FXML
+    private void handleDiv() {
+    	double tempNum = lastNum/Double.parseDouble(buffer);
+        String text = Double.toString(tempNum);
+    	display.setText(text);
+    	curr = text;
+        lastNum = tempNum;
+        buffer = "0";
+    }
+    
+    @FXML
+    private void handlePercent() {
+    	if(!isPercent) {
+    		double tempNum = Double.parseDouble(curr)/100;
+    		String text = Double.toString(tempNum)+"%";
+    		display.setText(text);
     	} else {
-    		num += button.getText();
+    		display.setText(curr);
     	}
+    }
+    
+    @FXML
+    private void handlePosNeg() {
+		double tempNum = -Double.parseDouble(curr);
+		String text = Double.toString(tempNum);
+		display.setText(text);
+		curr = text;
+        lastNum = tempNum;
+        buffer = "0";
+    }
+    
+    @FXML
+    private void handleClear() {
+    	display.setText("0");
+    	curr = "0";
+        lastNum = 0;
+        buffer = "0";
+    }
+    
+    @FXML
+    private void handleEqual() {
+    	System.out.println("I don't work yet.");
+    }
+    
+    @FXML
+    private void buttonBuffer(ActionEvent evt) {
+    	Button button = (Button)evt.getSource();
+    	buffer += button.getText();
     }
 }
---
+++ tests/tests/MainTest.java
@@ -0,0 +1,27 @@
+package tests;
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class MainTest {
+
+	@Before
+	public static void setUpBeforeClass() throws Exception {
+		
+	}
+
+	@After
+	public static void tearDownAfterClass() throws Exception {
+		
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
